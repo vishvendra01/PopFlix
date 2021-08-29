74dd61b6e5cc216473b9dcd8b8b5cf5d999c1be8
Vishvendra <vishvendra.singh@hotmail.com>
2021-08-30 02:25:55 +0530

    add viewmodel unit tests

--- app/src/test/java/com/app/newmoviesapp/ui/listing/MovieListingViewModelTest.kt
+++ app/src/test/java/com/app/newmoviesapp/ui/listing/MovieListingViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.app.newmoviesapp.ui.listing
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.app.newmoviesapp.data.entity.MovieEntity
 import com.app.newmoviesapp.data.executor.SchedulerProviderImpl
 import com.app.newmoviesapp.data.repository.MovieRepository
@@ -10,9 +11,9 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
+import org.junit.runner.RunWith
+import org.mockito.*
+import org.mockito.junit.MockitoJUnitRunner
 
 class MovieListingViewModelTest {
 
@@ -22,11 +23,17 @@ class MovieListingViewModelTest {
     @Mock
     lateinit var movieRepo: MovieRepository
 
+    @Captor
+    private lateinit var argumentCaptor: ArgumentCaptor<State<List<MovieEntity>>>
+
+    @Mock
+    private lateinit var observer: Observer<State<List<MovieEntity>>>
+
     private lateinit var moviesViewModel: MovieListingViewModel
 
     @Before
     fun setup() {
-        MockitoAnnotations.initMocks(this)
+        MockitoAnnotations.openMocks(this)
 
         moviesViewModel = MovieListingViewModel(
             movieRepo,
@@ -35,23 +42,67 @@ class MovieListingViewModelTest {
     }
 
     @Test
-    fun `testmovie load`() {
+    fun `should set progress state in live data at first`() {
         val sampleMovies = listOf<MovieEntity>(
-            MovieEntity(0, "Title", "fjjf", "fjkdj", 0.5, "2014", 1),
-            MovieEntity(1, "Title", "fjjf", "fjkdj", 0.5, "2014", 1),
-            MovieEntity(2, "Title", "fjjf", "fjkdj", 0.5, "2014", 1)
+            MovieEntity(0, "Title", "fjjf", "fjkdj", "", 0.5, "2014", 1),
+            MovieEntity(1, "Title", "fjjf", "fjkdj", "", 0.5, "2014", 1),
+            MovieEntity(2, "Title", "fjjf", "fjkdj", "", 0.5, "2014", 1)
         )
 
         Mockito.`when`(movieRepo.getMovies(Mockito.anyInt())).thenReturn(
             Flowable.just(sampleMovies)
         )
+
+        moviesViewModel.moviesLiveData.observeForever(observer)
+
         moviesViewModel.loadMovies(1)
 
-        Assert.assertTrue(moviesViewModel.moviesLiveData.value is State.Success)
-        val state = moviesViewModel.moviesLiveData.value
-        if(state is State.Success){
-            Assert.assertTrue(state.data.size == 3)
-        }
+        Mockito.verify(observer, Mockito.times(2)).onChanged(argumentCaptor.capture())
+        val values = argumentCaptor.allValues
+        Assert.assertTrue(values[0] is State.Loading)
+    }
+
+    @Test
+    fun `should set emitted movies in live data from repository`() {
+        val sampleMovies = listOf<MovieEntity>(
+            MovieEntity(0, "Title", "fjjf", "fjkdj", "", 0.5, "2014", 1),
+            MovieEntity(1, "Title", "fjjf", "fjkdj", "", 0.5, "2014", 1),
+            MovieEntity(2, "Title", "fjjf", "fjkdj", "", 0.5, "2014", 1)
+        )
+
+        Mockito.`when`(movieRepo.getMovies(Mockito.anyInt())).thenReturn(
+            Flowable.just(sampleMovies)
+        )
+
+        moviesViewModel.moviesLiveData.observeForever(observer)
+
+        moviesViewModel.loadMovies(1)
+
+        Mockito.verify(observer, Mockito.times(2)).onChanged(argumentCaptor.capture())
+        val values = argumentCaptor.allValues
+        Assert.assertTrue(values[1] is State.Success)
+
+        val successState = values[1] as State.Success
+
+        Assert.assertEquals(successState.data, sampleMovies)
+    }
+
+    @Test
+    fun `when repo returns error then state should be error`() {
+        Mockito.`when`(movieRepo.getMovies(Mockito.anyInt())).thenReturn(
+            Flowable.error(Throwable("error occurred"))
+        )
+
+        moviesViewModel.moviesLiveData.observeForever(observer)
+
+        moviesViewModel.loadMovies(1)
+
+        Mockito.verify(observer, Mockito.times(2)).onChanged(argumentCaptor.capture())
+        val values = argumentCaptor.allValues
+
+        Assert.assertTrue(values[0] is State.Loading)
+
+        Assert.assertTrue(values[1] is State.Error)
     }
 
 }
